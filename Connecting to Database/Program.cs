568d87f101cf2aa6135aa243b54a46de6fb076ac
Adithya <adithya.tadepalli@codebees.com>
2022-01-06 11:06:11 +0530

    C#

---
+++ Connecting to Database/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data.SqlClient;
+
+
+namespace Connecting_to_Database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString =
+            @"Data Source=ADITYA-CB-PC\SQLEXPRESS; Integrated Security=true; Initial Catalog = King";
+
+            string command = "Select * from dbo.employee";
+
+            using (SqlConnection conn=new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    SqlCommand comm = new SqlCommand(command, conn);
+                    using SqlDataReader reader = comm.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader[0]}, {reader[1]}, {reader[2]}");
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
---
+++ GenericClass/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+namespace GenericClass
+{
+    /*class GenericNumber<T>
+   // {
+    //    public T Name1; public T Name2;
+
+        public GenericNumber(T val1, T val2)
+        {
+            this.Name1 = val1;
+            this.Name2= val2;
+        }
+       
+    }
+    class Program
+    {
+        static void Main()
+        {
+            GenericNumber<string> Gen = new GenericNumber<string>("Aditya","Kerthi");
+         
+            Console.WriteLine("Name1 is\n"+ Gen.Name1);
+            Console.WriteLine("Name2 is\n"+ Gen.Name2);
+            
+        }
+    }*/
+
+    class program
+    {
+        static void Roman( string P)
+
+        { Console.WriteLine(P.ToUpper()); }
+
+        static void Main()
+        {
+            List<string> ns = new List<string>();
+            string s = Console.ReadLine();
+            while(s!= "Exit")
+            {
+                ns.Add(s);
+                s.Console.ReadLine();
+            }
+            ns.ForEach(Roman);
+        }
+    }
+}
