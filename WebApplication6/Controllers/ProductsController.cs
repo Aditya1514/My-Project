2aa962c44987e63e7c47fddb354fc5ea4cdf42f6
Adithya <adithya.tadepalli@codebees.com>
2022-01-17 12:55:47 +0530

    Models,ModelBinding,WorkingWithForms

---
+++ WebApplication6/Controllers/ProductsController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication6.Models;
+
+namespace WebApplication6
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            List<Product> Products = new List<Product>()
+            {
+                new Product() {ProductId =101, ProductName="Addon",ProductRate=1000,Productdate=12-1-21},
+                new Product() {ProductId = 102,ProductName="Trimmer",ProductRate=1200,Productdate=13-1-21},
+                new Product() {ProductId= 103,ProductName="Fogg",ProductRate=1500,Productdate=14-1-21},
+                new Product() {ProductId= 104,ProductName= "Pen",ProductRate= 10,Productdate=15-1-12}
+            };
+            return View(Products);
+        }
+        public ActionResult Details(int Id)
+        {
+            List<Product> Products = new List<Product>()
+            {
+                new Product() {ProductId =101, ProductName="Addon",ProductRate=1000,Productdate=12-1-21},
+                new Product() {ProductId = 102,ProductName="Trimmer",ProductRate=1200,Productdate=13-1-21},
+                new Product() {ProductId= 103,ProductName="Fogg",ProductRate=1500,Productdate=14-1-21},
+                new Product() {ProductId= 104,ProductName= "Pen",ProductRate= 10,Productdate=15-1-21}
+            };
+            Product matchingProduct = null;
+            
+                foreach (var item in Products)
+
+                {
+                    if (item.ProductId == Id)
+                    {
+                        matchingProduct = item;
+                    }
+                }
+
+               
+                return View(matchingProduct);
+            
+        }
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product A)
+        {
+            return View();
+        }
+
+
+    }
+     
+   
+}
---
+++ WebApplication6/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication6.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public double ProductRate { get; set; }
+        public int Productdate { get; set; }
+
+    }
+}
