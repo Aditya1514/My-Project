fd75f6680471a0e1845ab24ae18a9d793f2f5c4a
Adithya <adithya.tadepalli@codebees.com>
2022-01-17 15:44:48 +0530

    CustomModelBinders

---
+++ WebApplication8/Controllers/StudentController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication8.Models;
+namespace WebApplication8.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Create()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create([ModelBinder(typeof(CustomBinder))] Student st)
+        {
+            return View();
+        }
+    }
+
+    internal class CustomBinder
+    {
+    }
+}
---
+++ WebApplication8/Models/CustomBlinder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication8.Models
+{
+    public class CustomBlinder :IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext modelBindingContext)
+        {
+            int StudentId = Convert.ToInt32  (controllerContext.HttpContext.Request.Form["StudentId"]);
+            string StudentName = controllerContext.HttpContext.Request.Form["StudentName"];
+            string Doorno = controllerContext.HttpContext.Request.Form["Doorno"];
+            string street = controllerContext.HttpContext.Request.Form["street"];
+            string City = controllerContext.HttpContext.Request.Form["City"];
+
+            return new Student()
+            {
+                StudentId = StudentId,
+                StudentName = StudentName,
+                StudentAddress = Doorno + "," + street + "," + City
+            };
+
+        }
+    }
+}
