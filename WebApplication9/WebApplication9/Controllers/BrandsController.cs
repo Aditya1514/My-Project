bd2c9d8c3e4c2c2a34233bc4eab4d910162af997
Adithya <adithya.tadepalli@codebees.com>
2022-01-18 13:23:28 +0530

    Retriving All Rows & Single row

---
+++ WebApplication9/WebApplication9/Controllers/BrandsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication9.Models;
+namespace WebApplication9.Controllers
+{
+    public class BrandsController : Controller
+    {
+        // GET: Brands
+        public ActionResult Index()
+        {
+            KingFirstDatabaseEntities db = new KingFirstDatabaseEntities();
+            //Retriving All rows
+            List<Brands> Brands = db.Brands.ToList();
+            //Retriving Single row
+            // List<Brand> Brands =  db.Brands.Where(temp => temp.BrandName == "Samsung").ToList();
+            return View(Brands);
+        }
+    }
+}
---
+++ WebApplication9/WebApplication9/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication9.Models;
+namespace WebApplication9.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            KingFirstDatabaseEntities db = new KingFirstDatabaseEntities();
+            //Retriving all Rows.
+            List<Product> Produc = db.Products.ToList();
+            //Retriving Single row
+          //  List<Product> Produc= db.Products.Where(temp => temp.Price > 50000).ToList();
+            return View(Produc);
+        }
+    }
+}
