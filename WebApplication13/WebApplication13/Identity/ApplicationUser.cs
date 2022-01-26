41c128f1598dc9de13d65a60dec584922be13cc5
Adithya <adithya.tadepalli@codebees.com>
2022-01-26 12:41:32 +0530

    Identity Introduction

---
+++ WebApplication13/WebApplication13/Identity/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+namespace WebApplication13.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public DateTime? Birthday { get; set; }
+        public DateTime? Address { get; set; }
+        public DateTime? City { get; set; }
+    }
+}
