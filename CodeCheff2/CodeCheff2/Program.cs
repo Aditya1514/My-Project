13926e5ed1300f6534ecf92777a732b9a24a5c9e
Adithya <adithya.tadepalli@codebees.com>
2022-01-31 14:31:34 +0530

    Finding Sum Of Numbers

---
+++ CodeCheff2/CodeCheff2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CodeCheff2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 412, sum = 0;
+            while (number >0)
+            {
+                sum = sum + (number % 10);
+                number = number / 10;
+            }
+            Console.WriteLine("Sum of digits is {0}",sum);
+        }
+    }
+}
