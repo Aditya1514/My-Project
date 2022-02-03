b115b1482afe2aad613aa642cc9d609b1399546a
Adithya <adithya.tadepalli@codebees.com>
2022-02-03 10:20:05 +0530

    ReverseNumber

---
+++ CodeCheff3/Codecheff3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Codecheff3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int number = 123;
+            int remainder;
+
+            while (number > 0)
+            {
+                remainder = number % 10;
+                Console.WriteLine("{0}",remainder);
+                number = number / 10;
+            }
+
+        }
+    }
+}
