40e9ffcfe48ebe2ede7d53b0e936675c54a15b54
Adithya <adithya.tadepalli@codebees.com>
2022-02-04 12:42:49 +0530

    SquareRoot oF Number

---
+++ CodeCheff6/CodeCheff6/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CodeCheff6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 100;
+            Console.WriteLine("Square root of number is {0}",Math.Sqrt(x));
+            Console.ReadLine();
+        }
+    }
+}
