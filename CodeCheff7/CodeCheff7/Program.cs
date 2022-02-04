5f8eb7c07693aa2319484d5cf102ea495aef6806
Adithya <adithya.tadepalli@codebees.com>
2022-02-04 12:58:42 +0530

    Finding Remainder

---
+++ CodeCheff7/CodeCheff7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CodeCheff7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, div, rem;
+
+            Console.WriteLine("Enter a Number :");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a Divisor Number :");
+
+            div = Convert.ToInt32(Console.ReadLine());
+            if(num>div)
+            {
+                while(num>=div)
+                {
+                    num = num - div;
+                }
+                rem = num;
+                Console.WriteLine("Remainder is {0}" ,rem);
+            }
+            else {
+                Console.WriteLine("Enter a Valid Input");
+            }
+        }
+    }
+}
