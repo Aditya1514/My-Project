f4d55e1e7716f11deb3c6b8e803a452b335d6a44
Adithya <adithya.tadepalli@codebees.com>
2022-02-08 10:49:00 +0530

    SmallestPair

---
+++ CodeCheff8/Codecheff8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codecheff8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> answers = new List<int>();
+            int ARange = Convert.ToInt32(Console.ReadLine());
+            int InputRange = Convert.ToInt32(Console.ReadLine());
+            for(int i=0;i<ARange;i++)
+            {
+                string K = Console.ReadLine();
+                string[] inputval = K.Split(' ');
+                int[] Values = new int[inputval.Length];
+                for (int j =0; j<inputval.Length; j++)
+                {
+                    Values[j] += Convert.ToInt32(inputval[j]);
+                }
+                Array.Sort(Values);
+                answers.Add(Values[0] + Values[1]);
+            }
+            foreach (var item in answers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
